cae4dd19097fce6d95689b11fa21107069a26270
yhl221 <1727538058@qq.com>
2017-03-20 11:35:09 +0800

    add server.js and npm start

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('hello world');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening at port 3000：');
+});
