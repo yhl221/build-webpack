0554a694c955c43d7edf453b38b84afb68117d5f
yhl221 <1727538058@qq.com>
2017-03-20 11:37:30 +0800

    connect express and static html

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
+/*
 app.get('/', function(req, res){
     res.send('hello world');
 });
+*/
+
+app.use(express.static('./'));
 
 app.listen(3000, function () {
     console.log('Example app listening at port 3000：');
